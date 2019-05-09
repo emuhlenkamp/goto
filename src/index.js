6a757c44e84057fc707103469f51f118d184a3bc
emuhlenkamp <ericmuhlenkamp@yahoo.com>
2019-05-09 13:00:15 -0400

    added api js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const api = express();
+api.use(express.static(__dirname + '/public'));
+api.use(bodyParser.json());
+
 api.listen(3000, () => {
   console.log('API up and running!');
 });
@@ -9,3 +13,10 @@ api.get('/', (req, res) => {
   console.log(req);
   res.send('Hello, world!');
 });
+
+api.post('/add', (req, res) => {
+ console.log(req.body);
+ res.send('It works!');
+});
+
+// api.use(express.static(__dirname + '/public'));
