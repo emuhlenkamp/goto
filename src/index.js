50a37a9a965c656c2927ba0ceebdd4d80779c086
emuhlenkamp <ericmuhlenkamp@yahoo.com>
2019-05-10 15:45:41 -0400

    POOP

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 // const bodyParser = require('body-parser');
 
 const api = express();
+api.use(express.static(__dirname + '/public'));
+
 api.listen(3000, () => {
   console.log('API up and running!');
 });
-// api.use(express.static(__dirname + '/public'));
+
 // api.use(bodyParser.json());
 
 // api.use((req, res, next) => {
@@ -17,10 +19,11 @@ api.listen(3000, () => {
 //   console.log('API up and running!');
 // });
 
-api.get('/', (req, res) => {
-  console.log(req);
-  res.send('Hello, world!');
-});
+// api.get('/', (req, res) => {
+//   console.log(req);
+//   res.send('Hello, world!');
+//   next();
+// });
 
 // api.post('/add', (req, res) => {
 //  res.send('It works!');
