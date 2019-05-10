374dac65b90bfbb7efb965f3184b2a88365dc6df
emuhlenkamp <ericmuhlenkamp@yahoo.com>
2019-05-10 11:17:59 -0400

    hell yes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const api = express();
 api.use(express.static(__dirname + '/public'));
-api.use(bodyParser.json());
+// api.use(bodyParser.json());
 
 api.listen(3000, () => {
   console.log('API up and running!');
 });
 
-api.get('/', (req, res) => {
-  console.log(req);
-  res.send('Hello, world!');
-});
+// api.get('/', (req, res) => {
+//   console.log(req);
+//   res.send('Hello, world!');
+// });
 
-api.post('/add', (req, res) => {
- console.log(req.body);
- res.send('It works!');
-});
+// api.post('/add', (req, res) => {
+//  console.log(req.body);
+//  res.send('It works!');
+// });
 
 // api.use(express.static(__dirname + '/public'));
