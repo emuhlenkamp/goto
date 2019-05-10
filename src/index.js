50fadd5dc2e0bfb8065e942d475d9ef8f83fc688
emuhlenkamp <ericmuhlenkamp@yahoo.com>
2019-05-10 11:56:02 -0400

    yes

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ const api = express();
 // api.use(express.static(__dirname + '/public'));
 // api.use(bodyParser.json());
 
-api.use((req, res, next) => {
- console.log('Hello');
- next();
-});
-
-api.listen(3000, () => {
-  console.log('API up and running!');
-});
-
-// api.get('/', (req, res) => {
-//   console.log(req);
-//   res.send('Hello, world!');
+// api.use((req, res, next) => {
+//  console.log('Hello');
+//  next();
 // });
+
+// api.listen(3000, () => {
+//   console.log('API up and running!');
+// });
+
+api.get('/', (req, res) => {
+  console.log(req);
+  res.send('Hello, world!');
+});
 
 // api.post('/add', (req, res) => {
 //  res.send('It works!');
