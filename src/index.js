9e51185b188f0b163d27d0685c2a93235e6db1fc
emuhlenkamp <ericm@boomerangc.com>
2019-05-13 15:30:10 -0400

    serve

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 
-
-// const bodyParser = require('body-parser');
-
 const api = express();
+api.use(express.static(__dirname + '/public'));
+
+
 api.listen(3000, () => {
   console.log('API up and running!');
 });
 
-api.get('/', (req, res) => {
-  console.log(req);
-  res.send('Hello, world!');
-});
+//api.get('/', (req, res) => {
+//   console.log(req);
+//   res.send('Hello, world!');
+// });
 
-api.use(express.static(__dirname + '/public'));
 
+// const bodyParser = require('body-parser');
 
 // api.use(bodyParser.json());
 
