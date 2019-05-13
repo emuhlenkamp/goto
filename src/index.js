e19c0568b89257180d74ff620194a6785e2cd91f
emuhlenkamp <ericm@boomerangc.com>
2019-05-13 15:44:51 -0400

    test

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ api.listen(3000, () => {
   console.log('API up and running!');
 });
 
+api.post('/add', (req, res) => {
+ console.log('Post request received');
+});
+
 //api.get('/', (req, res) => {
 //   console.log(req);
 //   res.send('Hello, world!');
